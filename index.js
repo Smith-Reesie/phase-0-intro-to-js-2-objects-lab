f3191306c8e9f7c10f33501a6a169099ab84207d
Maurice Smith <Xmaurice.smith@gmail.com>
2021-09-07 19:23:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+// Write your solution in this file!//
+
+const employee = {
+    name:{},
+    streetAdress: {}
+    };
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+        return {
+          ...obj,
+          [key]: value,
+        };
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+     
+}
+function deleteFromEmployeeByKey(obj, key,) {
+    const newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key,) {
+    delete obj[key];
+    return obj;
+}
+
+
+
+
